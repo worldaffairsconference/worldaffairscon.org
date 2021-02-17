94d6306b3cb041435380c02723422d0cf831da3c
Jefferson Ding <tyding2004@outlook.com>
2021-02-16 23:42:26 -0500

    fix gh-pages single page application and modfield 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Card } from 'react-bootstrap';
 
 const NotFound = () => {
@@ -13,9 +14,9 @@ const NotFound = () => {
                 The page you&apos;re trying to visit is out of this world!
               </h1>
               <h3>
-                <a href="/index.html">
+                <Link to="/">
                   Maybe going back home will help you find your way.
-                </a>
+                </Link>
               </h3>
             </Card.Body>
           </Card>
