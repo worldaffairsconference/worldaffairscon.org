713a7ca70b31add76e169b386856ca9cdcaa317d
Jefferson Ding <tyding2004@outlook.com>
2021-04-08 01:48:10 -0400

    report web vitals to ga

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const sendToAnalytics = ({ id, name, value }) => {
+  window.gtag('event', name, {
+    event_category: 'Web Vitals',
+    event_value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    event_label: id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate
+  });
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL}>
@@ -17,4 +26,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(sendToAnalytics);
