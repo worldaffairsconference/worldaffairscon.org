fbc9772b4278cf07883108e3b3921c8eda5aec9d
Jefferson Ding <tyding2004@outlook.com>
2021-04-08 03:45:59 -0400

    remove GTM update web vitals

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const sendToAnalytics = ({ id, name, value }) => {
-  window.gtag('event', 'web_vital_report', {
-    name,
+  window.gtag('event', name, {
     event_category: 'Web Vitals',
     event_value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
     event_label: id, // id unique to current page load
