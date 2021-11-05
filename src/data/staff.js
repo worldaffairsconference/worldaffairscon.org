2c1e4635f5ebfd8a3a1782138defbde94805a413
Ibrahim Fadel <ibrahim.m.fadel@gmail.com>
2021-11-04 22:56:01 -0400

    update team page (excluding images / img cropping)

--- src/data/staff.js
+++ src/data/staff.js
@@ -4,72 +4,120 @@ const staff = {
       group: 'Chairs',
       members: [
         {
-          src: 'krbambawale.jpg',
-          name: 'Krishna Bambawale',
+          src: 'nknoth.jpg',
+          name: 'Nick Knoth',
           role: 'Conference Chair',
         },
-        { src: 'lmahony.jpg', name: 'Lauren Mahony', role: 'Conference Chair' },
+        {
+          src: 'sshepherd.jpg',
+          name: 'Sophie Shepherd',
+          role: 'Conference Chair'
+        },
+        {
+          src: 'jding.jpg',
+          name: 'Jefferson Ding',
+          role: 'Conference Chair'
+        },
+        {
+          src: 'lshatilla.jpg',
+          name: 'Lauren Shatilla',
+          role: 'Conference Chair'
+        },
       ],
     },
     {
       group: 'Executives',
       members: [
         {
-          src: 'kabambawale.jpg',
-          name: 'Karam Bambawale',
+          src: 'avatar.jpg',
+          name: 'Ian McCreadie',
           role: 'Head of SPRINT',
         },
-        { src: 'czhou.jpg', name: 'Catherine Zhou', role: 'Head of SPRINT' },
-        { src: 'bchan.jpg', name: 'Bill Chan', role: 'Head of Registration' },
         {
           src: 'avatar.jpg',
-          name: 'Jennifer Kwak',
-          role: 'Head of Registration &amp; Communications',
-        },
-        { src: 'mrivett.jpg', name: 'Max Rivett', role: 'Head of Technology' },
-        {
-          src: 'ssobie.jpg',
-          name: 'Sloan Sobie',
-          role: 'Head of Communications',
-        },
-        {
-          src: 'tsteininger.jpg',
-          name: 'Tayo Steininger',
-          role: 'Head of Registration &amp; Communications',
-        },
-        { src: 'pzhou.jpg', name: 'Princeton Zhou', role: 'Head of Plenaries' },
-        { src: 'ali.jpg', name: 'Amonda Li', role: 'Head of Plenaries' },
-        {
-          src: 'avatar.jpg',
-          name: 'Drake Belanger-Polak',
-          role: 'Head of Programs',
-        },
-        { src: 'ezhou.jpg', name: 'Emma Zhou', role: 'Head of Programs' },
-        {
-          src: 'lshatilla.jpg',
-          name: 'Lauren Shatilla',
-          role: 'Head of Programs',
-        },
-        { src: 'avatar.jpg', name: 'Jade Boucetta', role: 'Head of Security' },
-        {
-          src: 'avatar.jpg',
-          name: 'Anthony DiCriscio',
-          role: 'Head of Security',
-        },
-        { src: 'avatar.jpg', name: 'Eugene Ye', role: 'Head of Marketing' },
-        { src: 'avatar.jpg', name: 'Nick DeChazal', role: 'Head of Design' },
-        {
-          src: 'sshepherd.jpg',
-          name: 'Sophie Shepherd',
-          role: 'Head of Marketing and Design',
+          name: 'Michael Daiello',
+          role: 'Head of SPRINT'
         },
         {
           src: 'avatar.jpg',
-          name: 'Daniel Botros',
-          role: 'Head of Facilities',
+          name: 'Catherine Zhou',
+          role: 'Head of SPRINT'
         },
-        { src: 'avatar.jpg', name: 'Tafe Ajidagba', role: 'Head of Film' },
-        { src: 'dsharma.jpg', name: 'Dhruv Sharma', role: 'Head of Finance' },
+        {
+          src: 'avatar.jpg',
+          name: 'Will Huang',
+          role: 'Head of Plenaries'
+        },
+        {
+          src: 'slee.jpg',
+          name: 'Seungmyoung Lee',
+          role: 'Head of Plenaries',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Ibrahim Fadel',
+          role: 'Head of Technology'
+        },
+        {
+          src: 'jlee.jpg',
+          name: 'Justin Lee',
+          role: 'Head of Marketing'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Kashish Khatri',
+          role: 'Head of Marketing & Design'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Jack Fejer',
+          role: 'Head of Design'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Jamie Selfe',
+          role: 'Head of Finance'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Jack Zhu',
+          role: 'Head of Programs'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Miranda Yang',
+          role: 'Head of Programs'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Jayson Tian',
+          role: 'Head of Registration'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Vita Jackman Kuwabara',
+          role: 'Head of Registration'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Jason Gao',
+          role: 'Head of Production'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Robbie Tamblyn',
+          role: 'Head of Facilities'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Kalen Janmohamed',
+          role: 'Head of Security'
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Annie Kypreos',
+          role: 'Head of Security'
+        },
       ],
     },
     {
@@ -77,120 +125,157 @@ const staff = {
       members: [
         {
           src: 'avatar.jpg',
-          name: 'Ian McCreadie',
-          role: 'Assistant Head of SPRINT',
-        },
-        {
-          src: 'akypreos.jpg',
-          name: 'Annie Kypreos',
-          role: 'Assistant Head of SPRINT',
+          name: 'John Voudouris',
+          role: 'Assistant Head of Programs',
         },
         {
           src: 'avatar.jpg',
-          name: 'Olivia de Fazekas',
-          role: 'Assistant Head of SPRINT',
+          name: 'Amonda Li',
+          role: 'Assistant Head of Programs',
         },
         {
           src: 'avatar.jpg',
-          name: 'Lamia Tabassum',
-          role: 'Assistant Head of SPRINT',
-        },
-        {
-          src: 'myang.jpg',
-          name: 'Miranda Yang',
-          role: 'Executive Chair Assistant',
-        },
-        {
-          src: 'avatar.jpg',
-          name: 'Lewis Lapham',
-          role: 'Assistant Head of Registration',
-        },
-        {
-          src: 'ccai.jpg',
-          name: 'Christine Cai',
-          role: 'Assistant Head of Communication and Registration',
-        },
-        {
-          src: 'ssuleman.jpg',
-          name: 'Sofia Suleman',
-          role: 'Assistant Head of Communication and Registration',
-        },
-        {
-          src: 'avatar.jpg',
-          name: 'Ibrahim Fadel',
-          role: 'Assistant Head of Technology',
-        },
-        {
-          src: 'jding.jpg',
-          name: 'Jefferson Ding',
-          role: 'Assistant Head of Technology',
-        },
-        {
-          src: 'avatar.jpg',
-          name: 'Jack Zhu',
+          name: 'Rahul Nanda',
           role: 'Assistant Head of Plenaries',
         },
         {
-          src: 'aluo.jpg',
-          name: 'Angela Luo',
+          src: 'avatar.jpg',
+          name: 'Ore Maxwell',
           role: 'Assistant Head of Plenaries',
         },
-        { src: 'tmcdonald.jpg', name: 'Thea McDonald', role: 'Plenary Head' },
         {
           src: 'avatar.jpg',
-          name: 'AJ Shulman',
-          role: 'Assistant Head of Programs',
-        },
-        {
-          src: 'mmann.jpg',
-          name: 'Maylee Mann',
-          role: 'Assistant Head of Programs',
-        },
-        {
-          src: 'kkhatri.jpg',
-          name: 'Kashish Khatri',
-          role: 'Assistant Head of Programs',
-        },
-        {
-          src: 'jlee.jpg',
-          name: 'Justin Lee',
+          name: 'Aric Aggarwal',
           role: 'Assistant Head of Marketing',
         },
         {
           src: 'avatar.jpg',
-          name: 'Jason Gao',
-          role: 'Assistant Head of Design',
-        },
-        {
-          src: 'jmann.jpg',
-          name: 'Jamie Mann',
-          role: 'Assistant Head of Marketing and Design',
-        },
-        {
-          src: 'iahmed.jpg',
-          name: 'Izza Ahmed',
-          role: 'Assistant Head of Marketing and Design',
+          name: 'Bella Justo',
+          role: 'Assistant Head of Marketing & Design',
         },
         {
           src: 'avatar.jpg',
-          name: 'Nick Knoth',
-          role: 'Assistant Head of Facilities',
+          name: 'Ray Wu',
+          role: 'Assistant Head of Design',
         },
-        { src: 'avatar.jpg', name: 'Lucca Stanger', role: 'Opening Panel' },
-        { src: 'slee.jpg', name: 'Seungmyoung Lee', role: 'Opening Panel' },
-        { src: 'avatar.jpg', name: 'Michael Goralski', role: 'Closing Panel' },
-        { src: 'brusaw.jpg', name: 'Bella Rusaw', role: 'Closing Panel' },
-        { src: 'lworth.jpg', name: 'Lara Worth', role: 'Closing Panel' },
-        { src: 'avatar.jpg', name: 'Rahul Nanda', role: 'Plenary Head' },
-        { src: 'avatar.jpg', name: 'Robbie Evans', role: 'Plenary Head' },
-        { src: 'avatar.jpg', name: 'Will Huang', role: 'Plenary Head' },
-        { src: 'avatar.jpg', name: 'Dima Kulakov', role: 'Plenary Head' },
-        { src: 'psenst.jpg', name: 'Parker Senst', role: 'Plenary Head' },
-        { src: 'omaxwell.jpg', name: 'Ore Maxwell', role: 'Plenary Head' },
         {
-          src: 'vjackman.jpg',
-          name: 'Vita Jackman Kuwabara',
+          src: 'avatar.jpg',
+          name: 'Ray Wu',
+          role: 'Assistant Head of Design',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Ian Ye',
+          role: 'Assistant Head of Registration',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Thea McDonald',
+          role: 'Assistant Head of Registration',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Kevin Han',
+          role: 'Assistant Head of Finance',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Nate Finney',
+          role: 'Assistant Head of Technology',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Phil	Qin',
+          role: 'Assistant Head of Production',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Ray Wu',
+          role: 'Assistant Head of Design',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Eva Zeng',
+          role: 'Assistant Head of Design & Production',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Fatima 	Gohar',
+          role: 'Assistant Head of Communications',
+        },
+
+        {
+          src: 'avatar.jpg',
+          name: 'Kaelem	Moniz',
           role: 'Plenary Head',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Randy Chang',
+          role: 'Plenary Head',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Cole Herman',
+          role: 'Plenary Head',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Marshal	Wang',
+          role: 'Plenary Head',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Samantha	Schacter',
+          role: 'Plenary Head',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Natasha Yang',
+          role: 'Plenary Head',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Annie Deng',
+          role: 'Plenary Head',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Crystal Li',
+          role: 'Plenary Head',
+        },
+
+        {
+          src: 'avatar.jpg',
+          name: 'Jamie Mann',
+          role: 'Opening Panel',
+        },
+
+        {
+          src: 'avatar.jpg',
+          name: 'Cindy Han',
+          role: 'Closing Panel',
+        },
+
+        {
+          src: 'avatar.jpg',
+          name: 'Adam	Tarsky',
+          role: 'Ambassador Team',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Mateo Gjinali',
+          role: 'Ambassador Team',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Ryan	Chen',
+          role: 'Ambassador Team',
+        },
+        {
+          src: 'avatar.jpg',
+          name: 'Zohair	Pervez',
+          role: 'Ambassador Team',
         },
       ],
     },
