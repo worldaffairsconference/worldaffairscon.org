b6bbea3552139c49a952578f6d3d8980c1c9ff8d
Dwin2 <darwin.li@ucc.on.ca>
2022-10-01 13:50:17 -0400

    first batch of headshots

--- src/data/staff.js
+++ src/data/staff.js
@@ -14,12 +14,12 @@ const staff = {
           role: 'Conference Chair',
         },
         {
-          src: 'avatar.jpg',
+          src: 'slee.jpg',
           name: 'Seungmyoung Lee',
           role: 'Conference Chair',
         },
         {
-          src: 'avatar.jpg',
+          src: 'aismail.jpg',
           name: 'Amyna Ismail',
           role: 'Assistant Chair',
         },
@@ -39,17 +39,17 @@ const staff = {
           role: 'Head of Programs',
         },
         {
-          src: 'avatar.jpg',
+          src: 'jvoudouris.jpg',
           name: 'John Voudouris',
           role: 'Head of Plenaries',
         },
         {
-          src: 'avatar.jpg',
+          src: 'cli.jpg',
           name: 'Crystal Li',
           role: 'Head of Plenaries',
         },
         {
-          src: 'avatar.jpg',
+          src: 'axu.jpg',
           name: 'Alan Xu',
           role: 'Head of Keynotes & Panels',
         },
@@ -170,12 +170,12 @@ const staff = {
           role: 'Assistant Head of Design',
         },
         {
-          src: 'avatar.jpg',
+          src: 'ybhaloo.jpg',
           name: 'Yara Bhaloo',
           role: 'Assistant Head of Marketing & Design',
         },
         {
-          src: 'avatar.jpg',
+          src: 'lmoore.jpg',
           name: 'Leah Moore',
           role: 'Assistant Head of Communications & Registration',
         },
@@ -200,12 +200,12 @@ const staff = {
           role: 'Plenary Head',
         },
         {
-          src: 'avatar.jpg',
+          src: 'kma.jpg',
           name: 'Katherine Ma',
           role: 'Plenary Head',
         },
         {
-          src: 'avatar.jpg',
+          src: 'jzaki.jpg',
           name: 'Julia Zaki',
           role: 'Plenary Head',
         },
@@ -219,12 +219,12 @@ const staff = {
           name: 'Natalie Su',
           role: 'Plenary Head',
         },
-
         {
           src: 'avatar.jpg',
           name: 'Riley Skurka',
           role: 'Opening Panel',
         },
+        {},
         {
           src: 'avatar.jpg',
           name: 'Olivia Melchior',
