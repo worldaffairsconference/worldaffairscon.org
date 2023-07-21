7af4f82d3375583df709dde44ec0293e2b49cd90
Samuel-Martineau <samuel@smartineau.me>
2023-07-21 13:21:52 -0400

    fix: fix the page preview generation script

--- scripts/generatePagePreviews.mjs
+++ scripts/generatePagePreviews.mjs
@@ -1,7 +1,13 @@
+#!/usr/bin/env node
+
 import createManifestData from "@sveltejs/kit/create_manifest_data";
 import { load_config } from "@sveltejs/kit/config";
 import ts from "typescript";
 import puppeteer from "puppeteer";
+import path from "path";
+import url from "url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 function shouldPreviewPage(fileName) {
 	const program = ts.createProgram([fileName], {});
@@ -18,14 +24,14 @@ function shouldPreviewPage(fileName) {
 }
 
 const pagesToRender = createManifestData({
-	config: await load_config()
+	config: await load_config({ cwd: path.join(__dirname, "..") })
 })
 	.routes.filter((r) =>
 		r.leaf.universal ? shouldPreviewPage(r.leaf.universal) : false
 	)
 	.map((r) => ({
 		name: `page-preview${r.id.replaceAll("/", "-")}`,
-		path: r.id
+		url: r.id
 	}));
 
 const browser = await puppeteer.launch({
@@ -33,11 +39,19 @@ const browser = await puppeteer.launch({
 	defaultViewport: { width: 1536, height: 822 }
 });
 
-for (const { name, path } of pagesToRender) {
+for (const { name, url } of pagesToRender) {
 	const page = await browser.newPage();
-	await page.goto(`http://localhost:5173${path}`);
+	await page.goto(`http://localhost:5173${url}`);
 	await page.waitForNavigation({ waitUntil: "networkidle0" });
-	await page.screenshot({ path: `static/page-previews/${name}.png` });
+	await page.screenshot({
+		path: path.join(
+			__dirname,
+			"..",
+			"static",
+			"page-previews",
+			`${name}.png`
+		)
+	});
 	await page.close();
 }
 
