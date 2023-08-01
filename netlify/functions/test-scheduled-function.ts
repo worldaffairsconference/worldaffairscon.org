e387e4bc95a6965bc5986bd4979f6202e9ca6069
Samuel-Martineau <samuel@smartineau.me>
2023-08-01 19:18:32 -0400

    feat: tests with netlify functions

--- netlify/functions/test-scheduled-function.ts
+++ netlify/functions/test-scheduled-function.ts
@@ -1,11 +1,13 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 import { schedule } from "@netlify/functions";
+import { addEmailToDistributionList } from "$lib/server/zoho";
 
 const myHandler: Handler = async (
 	event: HandlerEvent,
 	context: HandlerContext
 ) => {
 	console.log("Received event:", event);
+	console.log(addEmailToDistributionList);
 
 	return {
 		statusCode: 200
