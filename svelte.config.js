a8f4ceeb4a1abc70d205ee375685aa58fc752c87
Samuel-Martineau <samuel@smartineau.me>
2023-08-03 12:34:37 -0400

    feat: move to custom netlify adapter to support scheduled functions and 404 page

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,7 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			// split: true
-		})
+		adapter: adapter()
 	}
 };
 
