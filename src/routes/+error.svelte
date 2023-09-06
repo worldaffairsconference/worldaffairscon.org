e3e03145d9c0b63df2616f395375c7cb420c76d7
principle105 <principle105@gmail.com>
2023-09-06 01:05:03 -0400

    refactor: mobile responsive 404 page + update preview

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -108,23 +108,27 @@
 
 <!-- TODO: refine this page and add other error handling -->
 {#if $page.status === 404}
-	<section class="absolute inset-0 z-10 bg-zinc-900">
+	<section
+		class="absolute inset-0 z-10 bg-zinc-900 h-screen w-screen overflow-hidden"
+	>
 		<div class="relative flex flex-col h-full w-full">
-			<div class="py-8 px-4 m-auto lg:px-6 text-center z-50">
+			<div class="py-8 px-10 m-auto lg:px-6 text-center z-50">
 				<h1
-					class="mb-2 text-7xl font-bold lg:text-[12rem] text-secondary pr-2.5"
+					class="mb-2 text-8xl font-bold md:text-9xl lg:text-[12rem] text-secondary lg:pr-2.5"
 				>
 					404
 				</h1>
-				<p class="mb-2 text-2xl font-bold md:text-4xl text-white">
+				<p
+					class="mb-2 text-xl font-bold md:text-3xl lg:text-4xl text-white"
+				>
 					It looks like you're lost...
 				</p>
-				<p class="mb-4 font-light text-zinc-400">
+				<p class="mb-4 font-light text-zinc-400 text-sm md:text-base">
 					the page you are looking for does not exist.
 				</p>
 				<a
 					href="/"
-					class="inline-flex text-white bg-gradient-to-r from-primary to-secondary rounded-lg text-sm px-5 py-3 text-center my-4 hover:brightness-[1.08] transition-all"
+					class="inline-flex text-white bg-gradient-to-r from-primary to-secondary rounded-lg text-xs md:text-sm px-5 py-3 text-center my-4 hover:brightness-[1.08] transition-all"
 				>
 					Go Back Home
 				</a>
