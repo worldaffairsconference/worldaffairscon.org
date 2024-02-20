bdc07cb782d96b6f4a1d1cfb0066539848b67bb4
Ray Wu <raymondwu0403@gmail.com>
2024-02-19 23:31:35 -0500

    fix: fix this ****shit

--- src/routes/(fullPage)/auth/auth-verify/+page.server.ts
+++ src/routes/(fullPage)/auth/auth-verify/+page.server.ts
@@ -1,9 +1,8 @@
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ parent, cookies }) => {
+export const load: PageServerLoad = async ({ parent }) => {
 	const { session } = await parent();
 	if (session?.user) throw redirect(303, "/dashboard");
-	console.log(cookies.getAll());
-	return { callbackUrl: cookies.get("authjs.callback-url") ?? "/" };
+	return {};
 };
