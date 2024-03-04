a8f02c55faca3f973c7b550a86ea862fc76018ec
Samuel-Martineau <samuel@smartineau.me>
2024-03-04 16:33:49 -0500

    feat: add privacy policy

---
+++ src/routes/(other)/privacy-policy/+page.svelte
@@ -0,0 +1,88 @@
+<h1>World Affairs Conference (WAC) Privacy Policy</h1>
+
+<h2>I. INTRODUCTION</h2>
+
+<p>
+	We, at World Affairs Conference (<em>WAC</em>), respect the privacy of our
+	participants and are committed to protecting their personal information.
+	This Privacy Policy describes how we collect, use, and protect personal
+	information provided by participants through our conference registration
+	website.
+</p>
+
+<h2>II. INFORMATION WE COLLECT</h2>
+
+<p>The types of personal information we collect about our attendees include:</p>
+<ul>
+	<li>Email Address</li>
+	<li>Email Verification Date</li>
+	<li>First Name & Last Name</li>
+	<li>Grade Level</li>
+	<li>School</li>
+	<li>Dietary Restrictions</li>
+	<li>Account Creation & Update Date</li>
+	<li>Plenary Preferences</li>
+</ul>
+
+<p>
+	This information is collected to facilitate the organization and successful
+	execution of the conference.
+</p>
+
+<h2>III. USE OF INFORMATION</h2>
+
+<p>
+	The aforementioned information is used for the purpose of conference
+	planning, organizing, communication, and ensuring dietary standards are met.
+	This usage is solely for the internal purposes of WAC.
+</p>
+
+<h2>IV. STORAGE & DELETION OF INFORMATION</h2>
+
+<p>
+	All personal information collected will be promptly deleted following the
+	conclusion of the conference. The collected personal information will also
+	be stored securely in a database located in the State of Virginia, United
+	States of America, provided by our partner, Xatabase, Inc.
+</p>
+
+<h2>V. DISCLOSURE OF INFORMATION</h2>
+
+<p>
+	We do not disclose, sell, or share the collected personal data with any
+	third parties, except for our database provider, Xatabase, Inc. This sharing
+	of data is strictly for the purpose of secure data storage and does not
+	involve any other use of the data by Xatabase, Inc.
+</p>
+
+<h2>VI. LOCATION & INTERNATIONAL DATA TRANSFER</h2>
+
+<p>
+	We are based in Toronto, Canada. However, please note that the data we
+	collect is stored by our database provider in the United States of America,
+	which causes an international data transfer to take place.
+</p>
+
+<h2>VII. CHANGES TO PRIVACY POLICY</h2>
+
+<p>
+	This privacy policy is subject to change. All changes will be posted on our
+	website in due course.
+</p>
+
+<h2>VIII. CONTACT INFORMATION</h2>
+
+<p>
+	If you have any questions or concerns about our privacy practices, or if you
+	wish to exercise your data protection rights, please contact us at: <a
+		href="mailto:registration@worldaffairscon.org"
+		>registration@worldaffairscon.org</a
+	>.
+</p>
+
+<p>
+	By registering for this conference, you consent to the collection, use, and
+	storage of your personal information as described in this privacy policy.
+</p>
+
+<p><b>Last updated:</b> 2024-03-04T21:24:07+0000</p>
