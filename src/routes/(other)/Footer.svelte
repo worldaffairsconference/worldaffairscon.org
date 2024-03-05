6130a8a25f1aef4f23586c75fbaac72210cd18ae
Samuel-Martineau <samuel@smartineau.me>
2024-03-05 15:29:46 -0500

    fix: add starter styles for privacy policy

--- src/routes/(other)/Footer.svelte
+++ src/routes/(other)/Footer.svelte
@@ -129,7 +129,8 @@
 			</div>
 		</div>
 		<span class="text-xs md:text-sm text-zinc-400 mb-3 sm:mb-0">
-			© 2024 Upper Canada College. All Rights Reserved.
+			© 2024 Upper Canada College. All Rights Reserved —
+			<a href="/privacy-policy"> Privacy Policy </a>
 		</span>
 	</div>
 </footer>
